142239a4d7044132b47151f1f1c171b0a18057bb
wbaleco <wbaleco@globo.com>
2015-08-30 16:33:56 -0300

    Finalização da aplicação

--- Receita federal/src/view/UsaFuncionario.java
+++ Receita federal/src/view/UsaFuncionario.java
@@ -6,6 +6,7 @@ package view;
 import javax.swing.JOptionPane;
 
 import model.Funcionario;
+import model.PessoaJuridica;
 
 /**
  * @author Walter
@@ -13,33 +14,47 @@ import model.Funcionario;
  */
 public class UsaFuncionario {
 
-	
 	public static void main(String[] args) {
 		Funcionario funcionario = new Funcionario();
-		
-//		PessoaJuridica func = new PessoaJuridica();
-		
-		String u,v,t,z;
+
+		PessoaJuridica func = new PessoaJuridica();
+
+		String u, v, t, z;
 		int x;
-		z = JOptionPane.showInputDialog("Digite 1 se o contribuinte for uma pessoa física \n Digite 2 se for pessoa jjurídica");
+		z = JOptionPane.showInputDialog(
+				"Digite 1 se o contribuinte for uma pessoa física \n Digite 2 se for pessoa jjurídica");
 		x = Integer.parseInt(z);
-		if(z.length()==1){
+		if (z.length() == 1) {
 			switch (x) {
 			case 1:
 				u = JOptionPane.showInputDialog("Informe o nome do contribuinte");
 				v = JOptionPane.showInputDialog("Informe o rg do contribuinte");
-				t = JOptionPane.showInputDialog("Digite o número de identificação do cartão do contribuinte");
+				t = JOptionPane.showInputDialog("Digite o número cartão de identificação do contribuinte");
+
 				funcionario.setNome(u);
 				funcionario.setRg(v);
 				funcionario.setCartao(t);
-				
+
 				System.out.println("Nome :" + funcionario.getNome());
 				System.out.println("Rg :" + funcionario.getRg());
 				System.out.println("Cartão :" + funcionario.getCartao());
-				
+
 				break;
+			case 2:
+				u = JOptionPane.showInputDialog("Informe o nome do contribuinte");
+				v = JOptionPane.showInputDialog("Informe o cnpj do contribuinte");
+				t = JOptionPane.showInputDialog("Digite o número do cartão de identificação do contribuinte");
+
+				funcionario.setNome(u);
+				func.setCnpj(v);
+				funcionario.setCartao(t);
+
+				System.out.println("Nome: " + funcionario.getNome());
+				System.out.println("Cnpj: " + func.getCnpj());
+				System.out.println("Cartão identificação: " + funcionario.getCartao() );
 
 			default:
+				JOptionPane.showMessageDialog(null, "Opção inválida");
 				break;
 			}
 		}
